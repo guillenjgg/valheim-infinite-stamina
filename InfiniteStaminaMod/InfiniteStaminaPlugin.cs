8a3f186c5b64aea82ca0b6611635ce1bc53cf314
devmail8181@gmail.com <devmail8181@gmail.com>
2026-04-12 17:48:30 -0600

    04122026: Initial project commit

---
+++ InfiniteStaminaMod/InfiniteStaminaPlugin.cs
@@ -0,0 +1,155 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace InfiniteStaminaMod
+{
+    [BepInPlugin(ModGuid, ModName, ModVersion)]
+    [BepInProcess("valheim.exe")]
+    public class InfiniteStaminaPlugin : BaseUnityPlugin
+    {
+        public const string ModGuid = "com.hex.infinitestamina";
+        public const string ModName = "Infinite Stamina";
+        public const string ModVersion = "1.0.0";
+
+        public static InfiniteStaminaPlugin Instance { get; private set; }
+
+        private Harmony _harmony;
+        private ConfigEntry<KeyboardShortcut> _toggleKey;
+        private ConfigEntry<bool> _modEnabled;
+        private float _lastMessageTime;
+        private static readonly KeyboardShortcut DefaultHotKey = new KeyboardShortcut(KeyCode.F7);
+
+        public bool IsInfiniteStaminaEnabled => _modEnabled != null && _modEnabled.Value;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            _toggleKey = Config.Bind(
+                "General",
+                "ToggleKey",
+                DefaultHotKey,
+                "Hotkey to toggle infinite stamina.");
+
+            _modEnabled = Config.Bind(
+                "General",
+                "Enabled",
+                false,
+                "Whether infinite stamina is enabled.");
+
+            _toggleKey.SettingChanged += OnToggleKeyChanged;
+
+            _harmony = new Harmony(ModGuid);
+            _harmony.PatchAll();
+
+            Logger.LogInfo($"{ModName} {ModVersion} loaded.");
+        }
+
+        private void Update()
+        {
+            if (!IsGameActive())
+            {
+                return;
+            }
+
+            var player = Player.m_localPlayer;
+
+            if (IsShortcutPressedAllowingExtraKeys(_toggleKey.Value))
+            {
+                _modEnabled.Value = !_modEnabled.Value;
+                bool isEnabled = _modEnabled.Value;
+
+                if (isEnabled)
+                {
+                    float missingStamina = player.GetMaxStamina() - player.GetStamina();
+                    if (missingStamina > 0f)
+                    {
+                        player.AddStamina(missingStamina);
+                    }
+                }
+
+                ShowStatus(isEnabled);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_toggleKey != null)
+            {
+                _toggleKey.SettingChanged -= OnToggleKeyChanged;
+            }
+
+            Instance = null;
+
+            _harmony?.UnpatchSelf();
+        }
+
+        private bool IsGameActive()
+        {
+            return Time.timeScale != 0f && Player.m_localPlayer != null;
+        }
+
+        private void ShowStatus(bool isEnabled)
+        {
+            if (Time.time - _lastMessageTime < 0.2f)
+            {
+                return;
+            }
+
+            _lastMessageTime = Time.time;
+
+            string message = isEnabled
+                ? "Infinite Stamina: ENABLED"
+                : "Infinite Stamina: DISABLED";
+
+            Logger.LogInfo(message);
+
+            if (MessageHud.instance != null)
+            {
+                MessageHud.instance.ShowMessage(
+                    MessageHud.MessageType.Center,
+                    message);
+            }
+        }
+
+        private void OnToggleKeyChanged(object sender, EventArgs e)
+        {
+            string message = $"Hotkey updated -> {_toggleKey.Value}";
+            Logger.LogInfo(message);
+
+            if (MessageHud.instance != null)
+            {
+                MessageHud.instance.ShowMessage(
+                    MessageHud.MessageType.Center,
+                    message);
+            }
+        }
+
+
+        private static bool IsShortcutPressedAllowingExtraKeys(KeyboardShortcut shortcut)
+        {
+            if (shortcut.MainKey == KeyCode.None)
+            {
+                return false;
+            }
+
+            if (!Input.GetKeyDown(shortcut.MainKey))
+            {
+                return false;
+            }
+
+            foreach (var modifier in shortcut.Modifiers)
+            {
+                if (!Input.GetKey(modifier))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ InfiniteStaminaMod/Patches/PlayerUseStaminaPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace InfiniteStaminaMod.Patches
+{
+    [HarmonyPatch(typeof(Player), nameof(Player.UseStamina))]
+    public static class PlayerUseStaminaPatch
+    {
+        [HarmonyPrefix]
+        private static bool PreventStaminaUse(Player __instance)
+        {
+            if (InfiniteStaminaPlugin.Instance == null || !InfiniteStaminaPlugin.Instance.IsInfiniteStaminaEnabled || __instance != Player.m_localPlayer)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
